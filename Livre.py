8e474a7619aa177c56aca5a79a2ddf30b7af3482
gbadoe <gbadoejunior96@gmail.com>
2022-05-16 09:55:26 +0000

    make it better

---
+++ Livre.py
@@ -0,0 +1,406 @@
+from os import abort
+from sqlite3 import IntegrityError
+from token import AT
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+import psycopg2
+# importation de flask
+
+
+##################################################################################################################################
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:JunioR2000@localhost:5432/esagapi'
+# chaine de connexion à la base de données
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+####################################################################################################################################
+
+
+
+
+####################################################################################################################################
+
+class Categorie(db.Model):
+    __tablename__ = 'categorie'
+    id = db.Column(db.Integer, primary_key=True)
+    libelle_categorie = db.Column(db.String, nullable=False)
+    livres = db.relationship('Livre', backref='categorie', lazy=True)
+
+    def __init__(self,libelle_categorie):
+        self.libelle_categorie = libelle_categorie
+        
+    def format(self):
+        return {
+            "id" : self.id,
+            "libelle_categorie" : self.libelle_categorie
+        }
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()    
+       
+
+
+
+# table Livre
+class Livre(db.Model):
+    __tablename__ = 'livre'
+    id = db.Column(db.Integer, primary_key=True)
+    isbn = db.Column(db.Integer, unique=True, nullable=False)
+    titre = db.Column(db.String(), nullable=False)
+    date_publication = db.Column(db.Date(), nullable=False)
+    auteur = db.Column(db.String(), nullable=False)
+    editeur = db.Column(db.String(), nullable=False)
+    categorie_id = db.Column(db.Integer, db.ForeignKey('categorie.id'))
+
+    def __init__(self, isbn, titre, date_publication, auteur, editeur, categorie_id):
+        self.isbn = isbn
+        self.titre = titre
+        self.date_publication = date_publication
+        self.auteur = auteur
+        self.editeur = editeur
+        self.categorie_id = categorie_id
+
+    def format(self):
+        return {
+            "id" : self.id,
+            "isbn" : self.isbn,
+            "titre" : self.titre,
+            "auteur" : self.auteur,
+            "date_publication" : self.date_publication,
+            "editeur" : self.editeur,
+            "categorie_id" : self.categorie_id
+       }
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()   
+
+
+db.create_all()
+
+
+
+####################################################################################################################################
+
+####################################################################################################################################
+                                                    
+                                                    #AJOUT DE LIVRE#
+
+@app.route('/ajout', methods=['POST'])
+def ajout_livre():
+    
+        livres = Livre.query.all()
+        body = request.get_json()
+        new_isbn = body.get('isbn')
+        new_titre = body.get('titre')
+        new_datepublication = body.get('date_publication')
+        new_auteur = body.get('auteur')
+        new_editeur = body.get('editeur')
+        new_categorie = body.get('categorie_id')
+    
+
+        livre = Livre(isbn=new_isbn, titre=new_titre,date_publication=new_datepublication, auteur=new_auteur, editeur=new_editeur, categorie_id=new_categorie)
+        livre.insert()
+
+        return jsonify({
+        'success': True,
+        'livre_id': livre.id,
+        'Nombre_livre': len(livres)
+     })
+
+    
+####################################################################################################################################
+
+
+####################################################################################################################################
+                                                #LISTER LES LIVRES#
+@app.route('/livre', methods=['GET'])
+def liste_livres():
+    livres = Livre.query.all()
+    livres_formatted = [livre.format() for livre in livres]
+   
+    return jsonify({
+
+     'success': True,     
+     'livre': livres_formatted,
+     'nombre_livre':  len(livres)
+    })
+     
+####################################################################################################################################
+
+
+
+####################################################################################################################################
+                                        # RECHERCHER UN LIVRE PAR SON ID
+@app.route('/livre/<int:id>', methods=['GET'])
+def selectionner_etudiant(id):
+    livre = Livre.query.get(id)
+    if livre:
+
+        return jsonify({
+
+            'success': True,
+            'livre': [livre.format()]
+        })
+    else:
+        return {'message': 'Ce livre n\'existe pas'}
+####################################################################################################################################
+
+
+####################################################################################################################################
+                                                # MODIFIER UN LIVRE
+@app.route('/modifier/<int:id>', methods=['PATCH'])
+def modifier_livre(id):
+    try:
+        livres = Livre.query.get(id)
+        body = request.get_json()
+        livres.isbn = body.get('isbn', None)
+        livres.titre = body.get('titre', None)
+        livres.date_publication = body.get('date_publication', None)
+        livres.auteur = body.get('auteur', None)
+        livres.editeur = body.get('auteur', None)
+        livres.categorie_id = body.get('categorie_id', None)
+
+        livres.update()
+
+    except AttributeError:
+        return jsonify({
+            'success': False,
+            'message': 'le livre que vous voulez modifier n\'existe pas'
+        })    
+
+    except TypeError:
+        return jsonify({
+            'success': False,
+            'message': 'Aucune reponse après votre requête'
+        })
+    else:
+        return jsonify({
+            'success': True,
+            'livre_id': id,
+            'message': 'livre modifié avec succes'
+        })
+####################################################################################################################################
+
+                                    # SUPPRIMER UN LIVRE
+
+
+@app.route('/supprimer/<int:id>', methods=['POST'])
+def delete_livre(id):
+    try:
+        livre = Livre.query.get(id)
+        livre.delete()
+        livres = Livre.query.all()
+
+    except AttributeError:
+        return jsonify({
+            'success': False,
+            'message': 'Ce livre que vous voulez supprimer n\'existe pas'
+        })
+    except TypeError:
+        return jsonify({
+            'success': False,
+            'message': 'Aucune reponse après votre requête'
+        })
+
+    else:
+        return jsonify({
+            'success': True,
+            'nombre_livres': [livre.format() for livre in livres],
+            'livre_id': id,
+            'nombre_livres': len(livres)
+        })
+####################################################################################################################################
+
+
+
+####################################################################################################################################
+                                            ##ERROHANDLER
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        'success': False,
+        'error': 400,
+        'message': 'Mauvaise requête'
+    }), 400
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({
+        'success': False,
+        'error': 500,
+        'message': 'erreur interne du serveur'
+    }), 500
+
+
+@app.errorhandler(405)
+def method_not_error(error):
+    return jsonify({
+        'success': False,
+        'error': 405,
+        'message': 'Method not allowed'
+    }), 405
+
+
+@app.errorhandler(422)
+def unprocessable_error(error):
+    return jsonify({
+        'success': False,
+        'error': 422,
+        'message': 'Méthode Non Autorisée'
+    }), 422
+
+@app.errorhandler(404)
+def Not_Found_error(error):
+    return jsonify({
+        'success': False,
+        'error': 404,
+        'message': 'Non trouvé'
+    }), 404
+
+
+####################################################################################################################################
+
+
+####################################################################################################################################
+                                            #AJOUT D'UNE CATEGORIE#
+@app.route('/ajout_categorie', methods=['POST'])
+def ajout_categorie():
+    categories=Categorie.query.all()
+    body = request.get_json()
+    new_libelle_categorie = body.get('libelle_categorie',None)
+    
+     
+    categorie = Categorie(libelle_categorie=new_libelle_categorie)
+    categorie.insert()
+    return jsonify({
+        'success': True,
+        'categorie_id': categorie.id,
+        'nombre categorie': len(categories)
+    })
+
+####################################################################################################################################
+
+
+####################################################################################################################################
+                                                #LISTER LES CATEGORIES#
+@app.route('/categorie', methods=['GET'])
+def liste_categories():
+    categories = Categorie.query.all()
+    categories_formatted = [categories.format() for categories in categories]
+    
+   
+    return jsonify({
+
+     'success': True,   
+     'categorie': categories_formatted,
+     'nombre_categorie': len(categories)
+    })
+    
+####################################################################################################################################
+
+
+####################################################################################################################################
+                                            # RECHERCHER UNE CATEGORIE PAR SON ID
+
+@app.route('/categorie/<int:id>', methods=['GET'])
+def selectionner_categorie(id):
+
+    
+        categorie = Categorie.query.get(id)
+        if categorie : 
+            return jsonify ({
+                'success' : True,
+                'categorie': [categorie.format()]
+            })
+        else:
+            return jsonify({
+                'message':'Cette categorie n\'existe pas'
+            })    
+    
+        
+        
+        
+####################################################################################################################################
+
+
+####################################################################################################################################
+                                        # MODIFIER LE LIBELLE D'UNE CATEGORIE
+@app.route('/modifier_categorie/<int:id>', methods=['PATCH'])
+def modifier_categorie(id):
+    try:
+        categories = Categorie.query.get(id)
+        body = request.get_json()
+        categories.libelle_categorie = body.get('libelle_categorie', None)
+
+        categories.update()
+
+    except AttributeError:
+        return jsonify({
+            'success': False,
+            'message': 'Cette categorie que vous voulez modifier n\'existe pas'
+        })
+    except TypeError:
+        return jsonify({
+            'success': False,
+            'message': 'Aucune reponse après votre requête'
+        })
+    else:    
+
+        return jsonify({
+        'success': True,
+        'categorie_id': id,
+        'message': 'libelle categorie modifié avec succes'
+        })
+####################################################################################################################################
+
+
+####################################################################################################################################
+                                                
+                                                # SUPPRIMER UNE CATEGORIE
+
+@app.route('/supprimer_categorie/<int:id>', methods=['POST'])
+def delete_categorie(id):
+    try:
+        categorie = Categorie.query.get(id)
+        categorie.delete()
+        categories = Categorie.query.all()
+    except AttributeError:
+        return jsonify({
+            'success': False,
+            'message': 'Cette categorie que vous voulez supprimer n\'existe pas'
+        })
+    except TypeError:
+        return jsonify({
+            'success': False,
+            'message': 'Aucune reponse après votre requête'
+        })
+    else:    
+        return jsonify({
+        'success': True,
+        'nombre_categories': [categorie.format() for categorie in categories],
+        'categorie_id': id,
+        'nombre_categories': len(categories)
+        })
+#################################################################################################################################### 
---
+++ Readme.md
@@ -0,0 +1,256 @@
+## BIBLIOTHEQUE API
+ DEBUT 
+## Installation des dépendances
+Python 3.10.0
+pip 21.3 from C:\Users\ADMIN\AppData\Local\Programs\Python\Python310\lib\site-packages\pip(python3.10)
+
+Suivez les instructions pour installer la dernière version de python pour votre plate-forme dans la documentation python https://packaging.python.org/en/latest/guides/installing-using-pip-and-virtual-environments/
+
+## Environnement virtuel
+
+Nous vous recommandons de travailler dans un environnement virtuel chaque fois que vous utilisez Python pour des projets. Cela permet de garder vos dépendances pour chaque projet séparées et organisées. Les instructions pour configurer un environnement virtuel pour votre plate-forme peuvent être trouvées dans les docs python https://packaging.python.org/en/latest/guides/installing-using-pip-and-virtual-environments/
+
+## Dépendances PIP
+
+Une fois que vous avez configuré et exécuté votre environnement virtuel, installez les dépendances en accédant au répertoire /API et en exécutant:
+
+* pip install -r requirement.txt
+ou
+* pip3 install -r requirement.txt
+
+Cela installera tous les packages requis que nous avons sélectionnés dans le fichier requirements.txt.
+
+## Dépendances des clés
+
+* Flask est un framework de microservices backend léger. Flask est nécessaire pour gérer les demandes et les réponses.
+https://flask.palletsprojects.com/en/2.1.x/
+
+* SQLAlchemy est la boîte à outils Python SQL et l'ORM que nous utiliserons pour gérer la base de données sqlite légère. Vous travaillerez principalement dans app.py et pourrez
+https://www.sqlalchemy.org/
+
+## Configuration de la base de données
+Avec Postgres en cours d'exécution, restaurez une base de données à l'aide du fichier bibliotheque_database.sql fourni. Depuis le dossier backend dans le terminal, exécutez:
+
+*psql bibliotheque_database < bibliotheque_database.sql*
+
+Exécution du serveur
+
+Dans le répertoire API, assurez-vous d'abord que vous travaillez avec votre environnement virtuel créé.
+
+Pour exécuter le serveur sous Linux ou Mac, exécutez:
+*export FLASK_APP=flaskr
+export FLASK_ENV=development
+flask run*
+
+Pour exécuter le serveur sous Windows, exécutez:
+*set FLASK_APP=flaskr
+set FLASK_ENV=development
+flask run*
+
+La définition de la variable FLASK_ENV sur développement détectera les modifications de fichiers et redémarrera le serveur automatiquement.
+
+La définition de la variable FLASK_APP sur flaskr indique à flask d'utiliser le répertoire flaskr et le fichier __init__.py pour rechercher l'application.
+
+## RÉFÉRENCE API
+
+*Démarrage
+URL de base: à l'heure actuelle, cette application ne peut être exécutée que localement et n'est pas hébergée en tant qu'URL de base. L'application principale est hébergée par défaut, http://localhost:5000 ; qui est défini comme proxy dans la configuration frontale.*
+
+## GESTION DES ERREURS
+
+Les erreurs sont renvoyées sous forme d'objets JSON au format suivant : { "success":False "error": 400 "message":"Bad request }
+
+L'API renverra quatre types d'erreurs en cas d'échec des requêtes : . 400 : Mauvaise requête . 500 : erreur interne du serveur . 422 : Impossible à traiter . 404 : Non trouvé
+
+POINTS FINAUX (END POINTS)
+
+## GET/livre
+
+    GÉNÉRAL:
+    Ce point de terminaison renvoie une liste d'objets livres, la valeur de réussite, le nombre total de livres.
+
+    "livre": [
+        {
+            "auteur": "Bernard Dadie",
+            "categorie_id": 13,
+            "date_publication": "Sun, 02 Jan 2022 00:00:00 GMT",
+            "editeur": "Bernard Dadie",
+            "id": 2,
+            "isbn": 1235,
+            "titre": "Pagne noir"
+        }
+    ],
+    "nombre_livre": 1,
+    "success": true
+    }
+
+## GET/categorie
+
+    GÉNÉRAL:
+    Ce point de terminaison renvoie une liste d'objets categories, la valeur de réussite, le nombre total de categories.
+    
+    {
+    "categorie": [
+        {
+            "id": 12,
+            "libelle_categorie": "Roman"
+        },
+        {
+            "id": 14,
+            "libelle_categorie": "Article"
+        },
+        {
+            "id": 15,
+            "libelle_categorie": "Dictionnaire"
+        },
+        {
+            "id": 16,
+            "libelle_categorie": "Document"
+        },
+        {
+            "id": 13,
+            "libelle_categorie": "Conte"
+        }
+    ],
+    "nombre_categorie": 5,
+    "success": true
+    }
+    
+    
+## POST/supprimer (livre_id)
+
+     GÉNÉRAL:
+    Supprimer l'usine de l'ID donné si elle existe. Renvoie l'identifiant du livre supprimée, la valeur de réussite, le nombre total de livres
+
+    {
+    "livre_id": 1,
+    "nombre_livres": 1,
+    "success": true
+    }
+
+
+## POST/supprimer_categorie (categorie_id)
+
+     GÉNÉRAL:
+    Supprimer l'usine de l'ID donné si elle existe. Renvoie l'identifiant de la categorie supprimée, la valeur de réussite, le nombre total de livres
+    
+    {
+    "categorie_id": 11,
+    "nombre_categories": 0,
+    "success": true
+    }
+
+## PATCH/modifier(livre_id)
+    GÉNÉRAL:
+    Ce point de terminaison est utilisé pour mettre à jour un livre donnée et
+     Nous retournons un livre que nous mettons à jour
+     
+    {
+    "livre_id": 2,
+    "message": "livre modifié avec succes",
+    "success": true
+    }
+
+
+## PATCH/modifier_categorie(categorie_id)
+    GÉNÉRAL:
+    Ce point de terminaison est utilisé pour mettre à jour un livre donnée et
+     Nous retournons un livre que nous mettons à jour
+     
+     {
+    "categorie_id": 13,
+    "message": "libelle categorie modifié avec succes",
+    "success": true
+    }
+
+## POST/ajout
+
+    GÉNÉRAL:
+     Ce point de terminaison permet de créer une nouveau livre 
+     Dans le cas de la création d'une nouvelle question :
+     On retourne l'ID du nouveau livre crée, le livre qui a été créée,  et le nombre de livres.
+     
+     {
+    "Nombre_livre": 3,
+    "livre_id": 9,
+    "success": true
+}
+
+## POST/ajout_categorie
+
+    GÉNÉRAL:
+     Ce point de terminaison permet de créer une nouvelle categorie
+     Dans le cas de la création d'une nouvelle question :
+     On retourne l'ID de la nouvelle categorie crée, la categorie qui a été créée,  et le nombre de livres.
+     
+     {
+    "categorie_id": 16,
+    "nombre categorie": 4,
+    "success": true
+}
+
+
+## GET/livre (livre_id)
+    
+    GÉNÉRAL:
+     Ce point de terminaison permet de rechercher le livre par son id
+     Dans le cas de la cette recherche :
+     On retourne l'ID du livre, la reussite de la requête
+     
+      "livre": [
+        {
+            "auteur": "Jean Robert",
+            "categorie_id": 15,
+            "date_publication": "Sun, 10 May 2020 00:00:00 GMT",
+            "editeur": "France Edition",
+            "id": 9,
+            "isbn": 1012,
+            "titre": "Larousse"
+        }
+    ],
+    "success": true
+    
+## GET/categorie (categorie_id)
+    
+    GÉNÉRAL:
+     Ce point de terminaison permet de rechercher la categorie par son id
+     Dans le cas de la cette recherche :
+     On retourne l'ID de la categorie, la reussite de la requête    
+     
+     {
+    "categorie": [
+        {
+            "id": 13,
+            "libelle_categorie": "Bande Dessiné"
+        }
+    ],
+        "success": true
+    }
+    
+    
+    
+    
+
+
+
+
+
+  
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
